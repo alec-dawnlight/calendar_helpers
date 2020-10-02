c7ec2161744bafd7347558d33540e4e2bf467004
Alec Hodgkinson <alec@dawnlight.com>
2020-10-01 23:33:58 -0700

    update

--- helpers.js
+++ helpers.js
@@ -1,16 +1,3 @@
-function httpGet(url){
-    let xmlHttp = new XMLHttpRequest();
-    xmlHttp.open("Get", url, false);
-    xmlHttp.send();
-    return xmlHttp.responseText
-}
-
-// time_req = httpGet("http://localhost:5000/api/time")
-// date = new Date(JSON.parse(time_req)['now'])
-
-// events_req = httpGet("http://localhost:5000/api/get_events/")
-events = JSON.parse(events_req)['data']
-
 const DAYS_IN_MONTH = {
     0: 31,
     1: 28,
